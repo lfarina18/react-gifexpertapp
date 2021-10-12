86340b5ff85454eb9ac2c6d187b43ccee4427aec
Leonardo Farina <leonardoagustinfarina@gmail.com>
2021-10-12 11:32:38 -0300

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samuraí X', 'Dragon Ball']
+    const [categories, setCategories] = useState([]);
+
+    // const handleAdd = () => {
+    //     // setCategories([...categories, 'HunterXHunter']);
+    //     setCategories(cats => [...cats, 'HunterXHunter']);
+    //     /*el setCategories puede tener un callback en el cual el primer argumento es el valor del estado anterior y luego tiene que regresar el nuevo estado */
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+
+            <ol>
+                {
+                    // categories.map(category => {
+                    //     return <li key={category}> {category} </li>
+                    //     /* el key no puede ser el indice y tiene que ser único */
+                    // })
+                    categories.map(category => (
+                        <GifGrid
+                            key={category}
+                            category={category} />
+                    ))
+                }
+            </ol>
+
+
+        </>
+    );
+}
+
